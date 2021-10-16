69ef217e82c9218eea61fada160299a387cb92a6
Wiandop <ggaleev2005@gmail.com>
2021-10-16 23:20:20 +0300

    Newly modified files have been added

---
+++ main.c
@@ -0,0 +1,35 @@
+#include "quadratic_equation.c"
+#include "tests/tests.c"
+
+/**
+ * OUTPUT_LANGUAGE = English
+ * PROJECT_NAME = Quadratic equation
+ * PROJECT_NUMBER = 1.0.0
+ * PROJECT_BRIEF = A program for calculating the roots of a quadratic equation
+ * OUTPUT_DIRECTORY = /doc
+ */
+
+/**
+ * @brief Entry point
+ * @detailed Execution of the program
+ * starts here.
+ * @see Ref
+ * solution_options
+ * get_values
+ * calculation_square
+ * output
+ * @return Program exit status
+ */
+
+int main() {
+    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+    solution_options solution;
+
+    get_values(&a, &b, &c);
+    solution = calculation_square(a, b, c, &x1, &x2);
+    output(x1, x2, solution);
+
+    run_all_tests();
+    printf("All tests passed!\n");
+    return 0;
+}
